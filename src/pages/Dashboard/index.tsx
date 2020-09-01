5c26817570e93895d6d1d81a3ed59591c6bfb354
pmqueiroz <pedroqueiroz593@gmail.com>
2020-09-01 18:47:51 -0300

    Chore: connected to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const url =
-	'https://avatars1.githubusercontent.com/u/54639269?s=460&u=9949b9f529f31bf748ddc9148c610462276b5bd2&v=4';
+interface Repository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+}
 
 const Dashboard: React.FunctionComponent = () => {
+	const [newRepo, setNewRepo] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepository(
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> {
+		event.preventDefault();
+
+		const response = await api.get<Repository>(`repos/${newRepo}`);
+
+		const repository = response.data;
+
+		setRepositories([...repositories, repository]);
+		setNewRepo('');
+	}
+
 	return (
 		<>
 			<img src={logoImg} alt="Github Explorer" />
 			<Title>Explore Repos on Github</Title>
 
-			<Form>
-				<input placeholder="Insert a repository name" />
+			<Form onSubmit={handleAddRepository}>
+				<input
+					value={newRepo}
+					onChange={e => setNewRepo(e.target.value)}
+					placeholder="Insert a repository name eg. pmqueiroz/proffy"
+				/>
 				<button type="submit">Search</button>
 			</Form>
 
 			<Repositories>
-				<a href="teste">
-					<img src={url} alt="Author" />
-					<div>
-						<strong>pmqueiroz/proffy</strong>
-						<p>
-							Online teaching platform made with React.js and React
-							Native using Typescript during the NLW (Next Level Week)
-							#02
-						</p>
-					</div>
+				{repositories.map(repository => (
+					<a key={repository.full_name} href="teste">
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
 
-					<FiChevronRight size={30} />
-				</a>
-				<a href="teste">
-					<img src={url} alt="Author" />
-					<div>
-						<strong>pmqueiroz/proffy</strong>
-						<p>
-							Online teaching platform made with React.js and React
-							Native using Typescript during the NLW (Next Level Week)
-							#02
-						</p>
-					</div>
-
-					<FiChevronRight size={30} />
-				</a>
-				<a href="teste">
-					<img src={url} alt="Author" />
-					<div>
-						<strong>pmqueiroz/proffy</strong>
-						<p>
-							Online teaching platform made with React.js and React
-							Native using Typescript during the NLW (Next Level Week)
-							#02
-						</p>
-					</div>
-
-					<FiChevronRight size={30} />
-				</a>
+						<FiChevronRight size={30} />
+					</a>
+				))}
 			</Repositories>
 		</>
 	);
